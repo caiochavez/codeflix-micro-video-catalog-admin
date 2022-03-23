cab7306f01c82ce9a4ddde5fbc194ec1d3b4ed56
caiochavez <caaiochavez@gmail.com>
2022-03-22 22:11:57 -0300

    seedwork: UUID Entity and UUID Error

---
+++ src/@seedwork/domain/value-object/unique-entity-id.ts
@@ -0,0 +1,17 @@
+import { randomUUID } from 'crypto'
+import InvalidUuidError from "../../errors/invalid-uuid.error"
+
+export default class UniqueEntityId {
+
+  constructor(public readonly id?: string) {
+    this.id = this.id || randomUUID()
+    this.validate()
+  }
+
+  private validate () {
+    const regexUUID = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
+    const isValid = this.id.match(regexUUID)
+    if (!isValid) throw new InvalidUuidError()
+  }
+
+}
